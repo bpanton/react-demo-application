e5d63a801410d7177c91e7f7c0e8b13e3fc43216
Blake Panton <blake.panton@libertymutual.com>
2017-10-31 17:27:45 -0400

    project cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const config = {
-    entry: './lib/components/index.js',
+    entry: ['babel-polyfill','./lib/components/index.js'],
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
